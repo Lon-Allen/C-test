8b7eec05a7541f860d33a92a2e305e22e071b59c
Water <18104372940@163.com>
2022-01-08 12:10:53 +0800

    Delete Ccode directory

--- Ccode/te.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-int main(){
-     printf("123");
-     int i=1,j=2;
-     int sum = i+j;
-     return 0;
- }
--- Ccode/testforconnect.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-
-int main(int argc, char const *argv[])
-{
-    printf("I hope everything will be ok");
-    return 0;
-}
